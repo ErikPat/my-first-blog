b71dee6e3a3ce2ed16af80d8006e6852dab446a9
ErikPat <57539773+ErikPat@users.noreply.github.com>
2019-11-11 12:34:06 -0600

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 from django.utils import timezone
 from django.shortcuts import render
-from models import Post
+from .models import Post
 
 # Create your views here.
 def post_list(request):
